2af26c4fc9027e1dc31c5fe7dd41fbaefeb65e55
Christoph Linder <christoph.linder@dvbern.ch>
2021-03-23 17:15:29 +0100

    fix javadoc generation

--- src/main/java/ch/dvbern/oss/junitbeanvalidationextension/ValidatorCustomizer.java
+++ src/main/java/ch/dvbern/oss/junitbeanvalidationextension/ValidatorCustomizer.java
@@ -1,7 +1,10 @@
 package ch.dvbern.oss.junitbeanvalidationextension;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
 /**
- * You may inject this parameter in your {@link @BeforeAll} test setup.
+ * You may inject this parameter in your {@link BeforeAll}/{@link BeforeEach} test setup.
  *
  * Customizations in beforeEach <b>replace</b> from beforeAll
  * for all test-method-invocations in the applicable test class.
@@ -11,7 +14,7 @@ package ch.dvbern.oss.junitbeanvalidationextension;
  * <pre>{@code
  *  @BeforeEach
  *  public void beforeEach(
- * 			ValidatorCustomizer customizer
+ * 		ValidatorCustomizer customizer
  * 	) {
  * 		customizer.customize(Customizations.usingConstraintValidator(new MyCustomConstraintValidator("Special Setup")));
  *    }
